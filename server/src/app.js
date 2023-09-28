35d2c4afaa592f291802334f48fcd7799eb0d001
arthurs14 <arthurcs426@gmail.com>
2023-09-28 15:05:48 -0700

    removed root directory node packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,16 @@
 // Packages
 const express = require("express");
 
+// Local File Imports
+const planetsRouter = require("./routes/planets/planets.router");
+
 /* --- START OF FILE --- */
 const app = express();
+
+// MIDDLEWARE
 app.use(express.json());
+
+// ROUTES
+app.use(planetsRouter);
 
 module.exports = app;
