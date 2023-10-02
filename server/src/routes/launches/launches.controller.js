bacafc1280a190af0cb7a365a04bc5a916fbecff
arthurs14 <arthurcs426@gmail.com>
2023-10-02 14:34:06 -0700

    added validation for post request data to be caught

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -12,7 +12,21 @@ function httpAddNewLaunch(req, res) {
   try {
     const launch = req.body;
 
+    if (
+      !launch.mission ||
+      !launch.rocket ||
+      !launch.launchDate ||
+      !launch.target
+    ) {
+      return res.status(400).json({
+        error: "Missing required launch property",
+      });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+      return res.status(400).json({ error: "Invalid launch date" });
+    }
 
     addNewLaunch(launch);
 
