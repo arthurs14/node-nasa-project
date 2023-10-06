dc7d812883d46a0237774489604f2d42720c8dd7
arthurs14 <arthurcs426@gmail.com>
2023-10-06 14:00:24 -0700

    we shoudl now be adding planets to our database if and only if it does not exist already

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
 
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
 
 function isHabitablePlanet(planet) {
   return (
@@ -24,24 +24,43 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data);
         }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on("end", async () => {
+        const totalPlanets = (await getAllPlanets()).length;
+        console.log(`${totalPlanets} habitable planets found!`);
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find();
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.keplerName,
+      },
+      {
+        keplerName: planet.keplerName,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (e) {
+    console.error(`Could not save planet ${e}`);
+  }
 }
 
 module.exports = { loadPlanetsData, getAllPlanets };
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,8 +1,8 @@
 const { getAllPlanets } = require("../../models/planets.model");
 
-function httpGetAllPlanets(req, res) {
+async function httpGetAllPlanets(req, res) {
   try {
-    return res.status(200).json(getAllPlanets());
+    return res.status(200).json(await getAllPlanets());
   } catch (e) {
     res.status(400).json({ status: "fail", message: e });
   }
